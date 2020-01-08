c3dd9a48ef982aad25e880ef505ddf682b215979
Andrew Bennett <bennetaw@mcmaster.ca>
2020-01-08 05:28:18 -0500

    icons

--- src/components/Inputs/Text/Text.js
+++ src/components/Inputs/Text/Text.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../../../icons/edit.svg';
 
 import './Text.scss'; 
 
 const Text = ({ style, label, labelid }) => (
   <div className='text' style={style}>
-    <label for={labelid} >{label}</label>
+    <label for={labelid}>{label}</label>
     <input type="text" name={labelid} id={labelid} />
+    <Logo className='icon' />
   </div>
 );
 
